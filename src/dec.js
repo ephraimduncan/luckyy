3af701bcd0e5386a9fa3263227417c1570636551
Ephraim Atta-Duncan <[ephraimduncan68@gmail.com]>
2020-06-22 17:28:05 +0000

    Changed Object Literals to Enhanced Object Literals

--- src/dec.js
+++ src/dec.js
@@ -2,7 +2,7 @@ const DecRandom = {
   /**
    * Function to convert an float to a specific number of decimal places
    */
-  dec: (num, decimal) => {
+  dec(num, decimal) {
     return Number(String(num).slice(0, 2 + decimal));
   },
 
@@ -10,7 +10,7 @@ const DecRandom = {
    * Return a random decimal number with a specified decimal place
    */
 
-  rand: (dp = 3, limit = 10) => {
+  rand(dp = 3, limit = 10) {
     return DecRandom.dec(Math.random() * limit, dp);
   },
 
@@ -18,7 +18,7 @@ const DecRandom = {
    * Return an array of random decimal numbers
    * with a specified number of decimal places
    */
-  randArray: (arrLen = 10, dp = 3, limit = 10) => {
+  randArray(arrLen = 10, dp = 3, limit = 10) {
     let randArr = [];
     for (let i = 0; i < arrLen; i++) {
       randArr.push(DecRandom.rand(dp, limit));
@@ -30,7 +30,7 @@ const DecRandom = {
    * Return an array of range of specified random decimal numbers
    * with a specified number of decimal places
    */
-  randArrayRange: (start, limit, arrLen = 10, dp = 3) => {
+  randArrayRange(start, limit, arrLen = 10, dp = 3) {
     const arr = () => {
       let randArr = [];
       for (let i = 0; i < arrLen; i++) {
--- src/rand.js
+++ src/rand.js
@@ -3,7 +3,7 @@ const Random = {
    * Return a single whole random number
    * Up to a specific number if stated
    */
-  rand: (limit = 10) => {
+  rand(limit = 10) {
     return Math.floor(Math.random() * limit);
   },
 
@@ -11,7 +11,7 @@ const Random = {
    * Return an array of random numbers from 0 to 10
    * Up to a specific number if stated
    */
-  randArray: (limit = 10, arrLen = 10) => {
+  randArray(limit = 10, arrLen = 10) {
     let randArr = [];
     for (let i = 0; i < arrLen; i++) {
       randArr.push(Random.rand(limit));
@@ -22,7 +22,7 @@ const Random = {
   /**
    * Return a set number of random numbers between a certain range Arrray
    */
-  randArrayRange: (start, limit = 10, arrLen = 10) => {
+  randArrayRange(start, limit = 10, arrLen = 10) {
     const arr = () => {
       let randArr = [];
       for (let i = 0; i < arrLen; i++) {
@@ -38,7 +38,7 @@ const Random = {
     try {
       return arr();
     } catch (RangeError) {
-      console.error("Call Stack Full. Retry Again");
+      console.error('Call Stack Full. Retry Again');
     }
   },
 };
